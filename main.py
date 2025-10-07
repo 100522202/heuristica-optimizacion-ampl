fab0cb322b0c938cff20ac2793c95dd3c4fafe58
100522300 <100522300@alumnos.uc3m.es>
2025-10-07 12:40:42 +0200

    Nuevas mejoras

--- main.py
+++ main.py
@@ -1,102 +1,40 @@
-import json  # Para leer el fichero JSON
-from pathlib import Path  # Para escribir el .dat
+import json, os
 
-#Pedimos el nombre del fichero JSON al usuario
-ruta_json = input("Introduce el nombre del fichero JSON")
+with open("datos.json", "r", encoding="utf-8") as f:
+    datos = json.load(f)
 
-#Leemos el JSON
-with open(ruta_json, "r", encoding="utf-8") as f:
-   datos = json.load(f)
+autobuses = datos["AUTOBUSES"]   #Obtiene los datos del json de autbous y devulve la lista correspondiente ["a1","a2","a3","a4","a5"]
+talleres  = datos["TALLERES"]    # Obtiene los datos del json de talleres y devulve la lista correspondiente["t1","t2","t3","t4","t5"]
+dist      = datos["DISTANCIA"]   # Obtiene los datos de diastancia del json dist["t1"]["a1"] = número
 
-#Extraemos los datos y la matriz  del json
-autobuses = datos["AUTOBUSES"]   # Lista: ["a1","a2",...]
-talleres = datos["TALLERES"]     # Lista: ["t1","t2",...]
-distancia = datos["DISTANCIA"]   # Diccionario: distancia["t1"]["a1"] = número
-
-#Nos aseguramos de que para cada taller haya autobuses
-# Si falta alguna distancia, lanzamos un mensaje de error.
-for t in talleres:
-   if t not in distancia:
-       raise ValueError("Falta la fila del taller '" + t + "' dentro de 'DISTANCIA' en el JSON.")
-   for a in autobuses:
-       if a not in distancia[t]:
-           raise ValueError("Falta la distancia para taller '" + t + "' y autobús '" + a + "' en 'DISTANCIA'.")
-
-# Empezamos a crear el contenido del .dat en texto
+#Creamos el texto que se encontrara dentro de nuestro .dat
 texto = ""
-#Conjunto de talleres
-texto += "set TALL := " + " ".join(talleres) + ";\n\n"
-#Conjunto de autobuses
-texto += "set AUT:= " + " ".join(autobuses) + ";\n\n"
-# Matriz de distancias: filas=talleres, columnas=autobuses
+texto += "set TALL := " + " ".join(talleres) + ";\n\n"#Añadimos a set TALL
+# la lista de talleres obtenida anteriormente del json
+texto += "set AUT := " + " ".join(autobuses) + ";\n\n"#Añadimos a set AUT
+# la lista de autobuses obtenida anteriormente del json
+#Aqui realizamos un bucle para rellenar la matriz que representa todas las
+# diastancias pero primero añadimos la lista de autbous para representar las
+# columnas de la matriz y luego añadiremos las filas con los talleres
 texto += "param DIST : " + " ".join(autobuses) + " :=\n"
-
 for t in talleres:
-   # Escribimos el nombre del taller
-   fila = t + " "
-   for a in autobuses:
-       #Añadimos las distancias del taller a cada autbous
-       fila += str(distancia[t][a]) + " "
-   # Guardamos la linea al final del texto
-   texto += "  " + fila + "\n"
-#Se añade al final un punto y coma marcando que se han añadido todas las filas
+    linea = t + " "
+    for a in autobuses:
+        linea += str(dist[t][a]) + " "
+    texto += "  " + linea + "\n"
+#Indicamos el final con un ;
 texto += ";\n"
 
-#Escribimos el archivo de salida .dat
-ruta_salida = Path("instancia.dat")
-with open(ruta_salida, "w", encoding="utf-8") as f:
-   f.write(texto)
+#Abrimos un archivo instancia,dat al que escribiremos todo el texto
+# genereado anteriormente
+with open("instancia.dat", "w", encoding="utf-8") as f:
+    f.write(texto)
 
-print("Fichero .dat generado correctamente:")
-print(ruta_salida.resolve())
-import json  # Para leer el fichero JSON
-from pathlib import Path  # Para escribir el .dat
+print("Generado instancia.dat")
 
-#Pedimos el nombre del fichero JSON al usuario
-ruta_json = input("Introduce el nombre del fichero JSON")
+#Resuelve el glpk con os.system que es similar a escribir en la terminal el
+# contenido descrito en la variable "comando"
+comando = "glpsol --model primerMOD.txt --data instancia.dat -o solucion.txt"
+os.system(comando)
 
-#Leemos el JSON
-with open(ruta_json, "r", encoding="utf-8") as f:
-   datos = json.load(f)
-
-#Extraemos los datos y la matriz  del json
-autobuses = datos["AUTOBUSES"]   # Lista: ["a1","a2",...]
-talleres = datos["TALLERES"]     # Lista: ["t1","t2",...]
-distancia = datos["DISTANCIA"]   # Diccionario: distancia["t1"]["a1"] = número
-
-#Nos aseguramos de que para cada taller haya autobuses
-# Si falta alguna distancia, lanzamos un mensaje de error.
-for t in talleres:
-   if t not in distancia:
-       raise ValueError("Falta la fila del taller '" + t + "' dentro de 'DISTANCIA' en el JSON.")
-   for a in autobuses:
-       if a not in distancia[t]:
-           raise ValueError("Falta la distancia para taller '" + t + "' y autobús '" + a + "' en 'DISTANCIA'.")
-
-# Empezamos a crear el contenido del .dat en texto
-texto = ""
-#Conjunto de talleres
-texto += "TALL := " + " ".join(talleres) + ";\n\n"
-#Conjunto de autobuses
-texto += "AUT:= " + " ".join(autobuses) + ";\n\n"
-# Matriz de distancias: filas=talleres, columnas=autobuses
-texto += "DIST : " + " ".join(autobuses) + " :=\n"
-
-for t in talleres:
-   # Escribimos el nombre del taller
-   fila = t + " "
-   for a in autobuses:
-       #Añadimos las distancias del taller a cada autbous
-       fila += str(distancia[t][a]) + " "
-   # Guardamos la linea al final del texto
-   texto += "  " + fila + "\n"
-#Se añade al final un punto y coma marcando que se han añadido todas las filas
-texto += ";\n"
-
-#Escribimos el archivo de salida .dat
-ruta_salida = Path("instancia.dat")
-with open(ruta_salida, "w", encoding="utf-8") as f:
-   f.write(texto)
-
-print("Fichero .dat generado correctamente:")
-print(ruta_salida.resolve()) #Muestra la ruta de salida
+print("GLPK ejecutado")
